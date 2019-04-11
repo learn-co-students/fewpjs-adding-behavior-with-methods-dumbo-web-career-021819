61b41c1f170ecf3feea6217f2fcfe8d0e8316694
github username <github email address>
2019-04-11 18:15:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Cat {
+  constructor(name) {
+    this.name = name
+  }
+  speak(){
+    return (this.name + ' says meow!')
+  }
+}
+
+class Dog {
+  constructor(name) {
+    this.name = name
+  }
+  speak(){
+    return (`${this.name} says woof!`)
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+    // bird's sex is male
+  }
+  // the function needs to be called inside of the class method so that it can be associated. If another class was to call the same function, it wouldn't apply UNLESS it is inside of that class method.
+
+  speak(){
+    if (this.sex === "male") {
+      return (`It's me! ${this.name}, the parrot!`)
+} else {
+    return `${this.name} says squawk!`
+  }
+// i'm only able to call on it bcuase it's a instance method
+  //   2) bird2.speak() returns 'Squawk!'
+  // *** if i put "static" in front, it becomes a class method
+  }
+}
+
+
+
+// arrow function
+// ***
